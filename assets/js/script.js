9bb91fa11c3ac585834b95188e1653728c2f96ab
brennaveir <brennaveir@hotmail.com>
2022-12-29 20:46:27 -0500

    added event listener and unfinished function for searching cities, added code to get current location

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,29 @@
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city
+var long;
+var lat;
+var requestUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=248ba8680dc03595a2d2c1b9765a1bdb"
+var searchBtn = document.getElementById("searchBtn")
+
+
+function getWeather() {
+
+}
+
+
+var successCallback = (position) => {
+    if (successCallback) {
+        long = position.coords.longitude;
+        lat = position.coords.latitude
+    };
+    console.log(requestUrl)
+}
+var errorCallback = (error) => {
+    console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+searchBtn.addEventListener('click', searchWeather)
+
